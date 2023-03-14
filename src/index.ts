46d9b2f306bb498178433dda7e7624a832037e03
mattisos <mattis.osula@funkdigital.ee>
2023-03-14 18:59:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
 import pg from "pg";
@@ -11,13 +10,30 @@ const port = process.env.PORT || 3333;
 app.use(cors());
 app.use(express.json());
 
+app.get("/treks/:trekId", async (req, res) => {
+  const sql = `SELECT * FROM treks WHERE id=${req.params.trekId};`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
+  res.send(JSON.stringify(rows[0]));
+});
+
 app.get("/treks", async (req, res) => {
+  const columns = req.query.columns;
   const status = req.query.status;
+  let sql = '';
+  let sqlColumns = '*';
   let sqlAdditionalFilters = '';
-  if (status !== undefined) {
-    sqlAdditionalFilters += `WHERE status = ${status}`
+
+  if (columns !== undefined) {
+    sqlColumns = columns.toString();
   }
-  const { rows } = await pool.query(`SELECT * FROM treks ${sqlAdditionalFilters};`);
+  if (status !== undefined) {
+    sqlAdditionalFilters += `WHERE status='${status}'`
+  }
+
+  sql = `SELECT ${sqlColumns} FROM treks ${sqlAdditionalFilters};`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
   res.send(JSON.stringify(rows));
 });
 
